ab3efcbb49f09ba907f5c2f1b4a2e7d7b784f452
s-nakajima <nakajimashouhei@gmail.com>
2023-02-24 22:13:23 +0900

    test: Github Actionエラー修正

--- Test/Case/AllCircularNoticesTest.php
+++ Test/Case/AllCircularNoticesTest.php
@@ -9,6 +9,8 @@
  * @copyright Copyright 2014, NetCommons Project
  */
 
+App::uses('NetCommonsTestSuite', 'NetCommons.TestSuite');
+
 /**
  * CircularNotices All Test Case
  *
@@ -25,7 +27,7 @@ class AllCircularNoticesTest extends CakeTestSuite {
  */
 	public static function suite() {
 		$plugin = preg_replace('/^All([\w]+)Test$/', '$1', __CLASS__);
-		$suite = new CakeTestSuite(sprintf('All %s Plugin tests', $plugin));
+		$suite = new NetCommonsTestSuite(sprintf('All %s Plugin tests', $plugin));
 		$suite->addTestDirectoryRecursive(CakePlugin::path($plugin) . 'Test' . DS . 'Case');
 		return $suite;
 	}
